e3f676637f125fa4e75868fe893fcb926f39630f
Overpeek <overpeek.fin@gmail.com>
2023-04-03 14:58:07 +0300

    surrealdb update

--- back/src/db/mod.rs
+++ back/src/db/mod.rs
@@ -1,27 +1,76 @@
-use std::{future::IntoFuture, sync::Arc};
-use surrealdb::{Datastore, Session};
-use util::{GetLazyAwait, LazyAwait};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use surrealdb::{
+    engine::local::{Db, Mem},
+    Surreal,
+};
+
+use crate::Message;
 
 //
 
-pub struct Database {
+/* pub struct Database {
     inner: Arc<LazyAwait<DatabaseInner>>,
-}
+} */
 
-#[derive(Debug, Clone)]
-struct DatabaseInner {
-    ds: Arc<Datastore>,
-    ses: Arc<Session>,
+#[derive(Clone)]
+pub struct Database {
+    db: Surreal<Db>,
 }
 
 //
 
-impl<'a> IntoFuture for &'a Database {
-    type Output = &'a DatabaseInner;
+impl Database {
+    pub async fn new() -> anyhow::Result<Self> {
+        let db = Surreal::new::<Mem>(()).await?;
+        db.use_ns("chat-app").use_db("chat-app").await?;
 
-    type IntoFuture = <LazyAwait<DatabaseInner> as IntoFuture>::IntoFuture;
-
-    fn into_future(self) -> Self::IntoFuture {
-        self.inner.as_ref().into_future()
+        Self { db }.init().await
     }
+
+    pub async fn send_msg(&self, msg: &str) -> anyhow::Result<Message> {
+        #[derive(Serialize)]
+        struct CreateMsg<'a> {
+            date: DateTime<Utc>,
+            content: &'a str,
+        }
+
+        let res: Message = self
+            .db
+            .create("messages")
+            .content(CreateMsg {
+                date: Utc::now(),
+                content: msg,
+            })
+            .await?;
+
+        Ok(res)
+    }
+
+    pub async fn get_msg(&self) -> anyhow::Result<Vec<Message>> {
+        const Q: &str = "SELECT * FROM messages ORDER BY date DESC LIMIT 10";
+
+        let msg: Vec<Message> = self.db.query(Q).await?.take(0)?;
+
+        Ok(msg)
+    }
+
+    async fn init(self) -> anyhow::Result<Self> {
+        /* const Q: &str = r#"CREATE TABLE messages (
+            id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY
+        )"#;
+
+        self.exec(Q, None).await?; */
+
+        Ok(self)
+    }
+
+    /* async fn exec(
+        &self,
+        query: &str,
+        vars: Option<BTreeMap<String, Value>>,
+    ) -> anyhow::Result<Vec<Response>> {
+        let res = self.ds.execute(query, &self.se, vars, false).await?;
+        Ok(res)
+    } */
 }
--- back/src/main.rs
+++ back/src/main.rs
@@ -1,55 +1,91 @@
+use self::db::Database;
 use axum::{
     extract::State,
     http::StatusCode,
-    response::Sse,
+    response::{
+        sse::{Event, KeepAlive},
+        Sse,
+    },
     routing::{get, post},
     Json, Router, Server,
 };
+use chrono::{DateTime, Utc};
+use futures::Stream;
+use serde::{Deserialize, Serialize};
 use std::{
+    convert::Infallible,
     net::{IpAddr, Ipv4Addr, SocketAddr},
-    sync::Arc,
 };
-use tokio::sync::RwLock;
-// use self::db::Database;
+use tokio::sync::broadcast::{channel, Sender};
 
 //
 
-// mod db;
+mod db;
 
-// #[derive(Clone)]
-// struct AppState {
-//     db: Database,
-// }
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    id: String,
+    date: DateTime<Utc>,
+    content: String,
+}
+
+#[derive(Clone)]
+struct AppState {
+    db: Database,
+    tx: Sender<Message>,
+}
 
 //
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let router = Router::new()
         .route("/", get(root))
         .route("/", post(set_root))
-        .with_state(Arc::new(RwLock::new("Hello, world!".into())));
+        .route("/sse", get(listen))
+        .with_state(AppState {
+            db: Database::new().await?,
+            tx: channel(1).0,
+        });
 
-    let server = Server::bind(&SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 0))
-        .serve(router.into_make_service());
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8080);
+
+    let server = Server::bind(&addr).serve(router.into_make_service());
     println!("Listening on http://{}/", server.local_addr());
 
-    server.await.unwrap();
+    server.await?;
+
+    Ok(())
 }
 
 async fn set_root(
-    State(state): State<Arc<RwLock<String>>>,
+    State(state): State<AppState>,
     Json(val): Json<String>,
 ) -> (StatusCode, Json<()>) {
-    *state.write().await = val;
+    let msg = state.db.send_msg(&val).await.unwrap();
+    state.tx.send(msg).unwrap();
 
     (StatusCode::CREATED, Json(()))
 }
 
-async fn root(State(state): State<Arc<RwLock<String>>>) -> String {
-    state.read().await.clone()
+async fn root(State(state): State<AppState>) -> Json<Vec<Message>> {
+    Json(state.db.get_msg().await.unwrap())
 }
 
-/* async fn listen() -> Sse {
-    axum::response::sse;
-} */
+async fn listen(
+    State(state): State<AppState>,
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    let mut rx = state.tx.subscribe();
+
+    let stream = async_stream::stream! {
+        while let Ok(v) = rx.recv().await {
+            yield Ok(Event::default().json_data(v).unwrap())
+        }
+    };
+
+    // axum::response::sse;
+
+    Sse::new(stream).keep_alive(KeepAlive::default())
+}
